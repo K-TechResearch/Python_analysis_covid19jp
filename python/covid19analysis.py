392e0c3d3f9720563bcb1c9353ce01e7a19d5fba
toshi-tahara <dummy@example.com>
2020-07-15 14:38:11 +0900

    csvの列名変更対応

--- python/covid19analysis.py
+++ python/covid19analysis.py
@@ -10,7 +10,7 @@ summary = pd.read_csv('https://github.com/kaz-ogiwara/covid19/raw/master/data/su
 prefectures = pd.read_csv('https://github.com/kaz-ogiwara/covid19/raw/master/data/prefectures.csv') #copyright TOYO KEIZAI ONLINE
 csv_name = ['summary','prefecture']
 #check
-print(prefectures['prefectureNameE'])
+print(prefectures.iloc[:,[0,1,2]])
 
 #%%
 #diff時のエラー処理のため文字列の列を抽出しリスト化
@@ -52,7 +52,7 @@ osaka = prefectures[prefectures['prefectureNameE'].isin([pre_pick[1]])]
 osaka['prefectureNameE'] = pd.to_numeric(osaka['prefectureNameE'] , errors = 'coerce')
 aichi = prefectures[prefectures['prefectureNameE'].isin([pre_pick[2]])]
 aichi['prefectureNameE'] = pd.to_numeric(aichi['prefectureNameE'] , errors = 'coerce')
-
+tokyo
 #%%
 #summary差分dataframe
 summary_diff = summary.diff() #日差分データ
@@ -64,21 +64,27 @@ osaka_diff7 = osaka.diff(7)
 aichi_diff7 = aichi.diff(7)
 #check
 print(osaka_diff7)
-
+print(osaka_diff7.columns)
 #%%
 #列のインデックス(行は年月日)
-#年,月,日,pcr_tested_positive,pcr_tested,有症状者,無症状者,症状有無確認中,入院治療を要する者,入院治療を要する者（無症状）,退院者,退院者（突合作業中を含む）,人工呼吸器又は集中治療室に入院している者,死亡者,死亡者（突合作業中を含む）,PCR検査数：国立感染症研究所,PCR検査数：検疫所,PCR検査数：地方衛生研究所・保健所,PCR検査数：民間検査会社,PCR検査数：民間検査会社のうち保険適用分,PCR検査数：大学等,PCR検査数：大学等のうち保険適用分,PCR検査数：医療機関,PCR検査数：医療機関のうち保険適用分,pcr_test_total,PCR検査数：保険適用分の合計,URL
-
 #解析用dataframe作成
 fig = plt.figure() #plot initializing
 fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(9, 6)) #plot area
 #summary
 #陽性/PCR人数 1日差分だと誤差大のため7日平均で確認
-summary_diff7['yosei/PCR_7day'] = summary_diff7['pcr_tested_positive'] / summary_diff7['pcr_tested']
-summary_diff7['yosei/PCR_N_7day'] = summary_diff7['pcr_tested_positive'] / summary_diff7['pcr_tests_total']
+summary_diff7['yosei/PCR_7day'] = summary_diff7['tested_positive'] / summary_diff7['people_tested']
+summary_diff7['yosei/PCR_N_7day'] = summary_diff7['tested_positive'] / summary_diff7['pcr_tests_total']
 summary_diff7[['yosei/PCR_7day','yosei/PCR_N_7day']].plot(ax = axes[0,0] , kind = 'line' , grid=True , legend =True)
 #PCR人数と回数
-summary_diff7[['pcr_tested','pcr_tests_total']].plot(ax = axes[1,0] , kind = 'line' , grid=True , legend =True)
-
+summary_diff7[['people_tested','pcr_tests_total']].plot(ax = axes[1,0] , kind = 'line' , grid=True , legend =True)
+#%%
+#県別比較_ave7daysを新設
+ave7days = summary_diff7
+ave7days['testedPositive'] = summary_diff7['tested_positive']/7
+ave7days.rename(columns={'testedPositive' : 'Alljp'}, inplace = True)
+ave7days['tokyo'] = tokyo_diff7['testedPositive']/7
+ave7days['osaka'] = osaka_diff7['testedPositive']/7
+ave7days['aichi'] = aichi_diff7['testedPositive']/7
+ave7days[['Alljp','tokyo','osaka','aichi']].plot(ax = axes[0,1] , kind = 'line' , grid=True , legend =True)
 #%%
 plt.show()
